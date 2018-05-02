70de0dd33bfcbd55b3551dfb69e1e4a39ce8513f
DarjaJestin <jestin.darja@gmail.com>
2018-05-02 20:24:52 +0300

    Added layouts for product

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'main/index'
-
   root 'main#index'
+
+  resources :products
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
